1c37d8057d028a7f4f3076a35333e15d17d00b87
Bryant Moreira dos Anjos <bryant_mx@hotmail.com>
2020-07-20 01:34:46 -0300

    Added functionality to prevent users from making mutations without passing through any required fields

--- server/src/schema/index.ts
+++ server/src/schema/index.ts
@@ -5,6 +5,7 @@ import {
   GraphQLList,
   GraphQLString,
   GraphQLInt,
+  GraphQLNonNull,
 } from 'graphql'
 
 import { BookType, Book } from './book.schema'
@@ -61,8 +62,8 @@ const Mutation = new GraphQLObjectType({
     addAuthor: {
       type: AuthorType,
       args: {
-        name: { type: GraphQLString },
-        age: { type: GraphQLInt },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
       },
       resolve(parent, args) {
         const author = new AuthorSchema({
@@ -75,9 +76,9 @@ const Mutation = new GraphQLObjectType({
     addBook: {
       type: BookType,
       args: {
-        name: { type: GraphQLString },
-        genre: { type: GraphQLString },
-        authorId: { type: GraphQLID },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        genre: { type: new GraphQLNonNull(GraphQLString) },
+        authorId: { type: new GraphQLNonNull(GraphQLID) },
       },
       resolve(parent, args) {
         const book = new BookSchema({
